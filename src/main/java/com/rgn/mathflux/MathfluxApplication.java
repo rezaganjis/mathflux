b434b1d6e3c2031aed091fcf49c52d68d86c2287
rezaganjis <Ganji@tosan.com>
2023-11-06 12:28:02 +0330

    create project.

---
+++ src/main/java/com/rgn/mathflux/MathfluxApplication.java
@@ -0,0 +1,13 @@
+package com.rgn.mathflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MathfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MathfluxApplication.class, args);
+	}
+
+}
